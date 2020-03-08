6e98393ebe320cbf9e020f1607d7e4d5eb3d632a
hunnx27 <hunnx27@gmail.com>
2020-03-09 03:57:56 +0900

    all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TOC from "./components/TOC"
+import Subject from "./components/Subject"
+import Content from "./components/Content"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup
+        Language."></Content>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
